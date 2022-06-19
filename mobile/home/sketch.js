2c8920efcd748d1ca5622f6e92046fcf14db85c9
AmEyCoDeR <99733292+AmEyCoDeR@users.noreply.github.com>
2022-06-19 14:07:12 +0530

    Update sketch.js

--- mobile/home/sketch.js
+++ mobile/home/sketch.js
@@ -16,46 +16,46 @@ function imgdec() {
 
 function update() {
     if (imagenumber === 1) {
-        document.getElementById("img-div-desk").innerHTML = `<img src="img1.jpg" width="100vh">`;
+        document.getElementById("img-div-desk").innerHTML = `<img src="img1.jpg" height="100vh">`;
     }
     else if (imagenumber === 2) {
-        document.getElementById("img-div-desk").innerHTML = `<img src="img2.jpg" width="100vh">`;
+        document.getElementById("img-div-desk").innerHTML = `<img src="img2.jpg" height="100vh">`;
     }
     else if (imagenumber === 3) {
-        document.getElementById("img-div-desk").innerHTML = `<img src="img3.jpg" width="100vh">`
+        document.getElementById("img-div-desk").innerHTML = `<img src="img3.jpg" height="100vh">`
     }
     else if (imagenumber === 4) {
-        document.getElementById("img-div-desk").innerHTML = `<img src="img4.jpg" width="100vh">`
+        document.getElementById("img-div-desk").innerHTML = `<img src="img4.jpg" height="100vh">`
     }
     else if (imagenumber === 5) {
-        document.getElementById("img-div-desk").innerHTML = `<img src="img5.jpg" width="100vh">`
+        document.getElementById("img-div-desk").innerHTML = `<img src="img5.jpg" height="100vh">`
     }
     else if (imagenumber === 6) {
-        document.getElementById("img-div-desk").innerHTML = `<img src="img6.jpg" width="100vh">`
+        document.getElementById("img-div-desk").innerHTML = `<img src="img6.jpg" height="100vh">`
     }
     else if (imagenumber === 7) {
-        document.getElementById("img-div-desk").innerHTML = `<img src="img7.jpg" width="100vh">`
+        document.getElementById("img-div-desk").innerHTML = `<img src="img7.jpg" height="100vh">`
     }
     else if (imagenumber === 8) {
-        document.getElementById("img-div-desk").innerHTML = `<img src="img8.jpg" width="100vh">`
+        document.getElementById("img-div-desk").innerHTML = `<img src="img8.jpg" height="100vh">`
     }
     else if (imagenumber === 9) {
-        document.getElementById("img-div-desk").innerHTML = `<img src="img9.jpg" width="100vh">`
+        document.getElementById("img-div-desk").innerHTML = `<img src="img9.jpg" height="100vh">`
     }
     else if (imagenumber === 10) {
-        document.getElementById("img-div-desk").innerHTML = `<img src="img10.jpg" width="100vh">`
+        document.getElementById("img-div-desk").innerHTML = `<img src="img10.jpg" height="100vh">`
     }
     else if (imagenumber === 11) {
-        document.getElementById("img-div-desk").innerHTML = `<img src="img11.jpg" width="100vh">`
+        document.getElementById("img-div-desk").innerHTML = `<img src="img11.jpg" height="100vh">`
     }
     else if (imagenumber === 12) {
-        document.getElementById("img-div-desk").innerHTML = `<img src="img12.jpg" width="100vh">`
+        document.getElementById("img-div-desk").innerHTML = `<img src="img12.jpg" height="100vh">`
     }
     else if (imagenumber === 13) {
-        document.getElementById("img-div-desk").innerHTML = `<img src="img13.jpg" width="100vh">`
+        document.getElementById("img-div-desk").innerHTML = `<img src="img13.jpg" height="100vh">`
     }
     else if (imagenumber === 14) {
-        document.getElementById("img-div-desk").innerHTML = `<img src="img14.jpg" width="100vh">`
+        document.getElementById("img-div-desk").innerHTML = `<img src="img14.jpg" height="100vh">`
     }
     else {
         if (dec) {
